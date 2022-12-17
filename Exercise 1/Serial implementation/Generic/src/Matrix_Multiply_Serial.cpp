1d514dd8dc4b8ea9c59873f333cf544e68fb741f
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2022-12-17 19:34:08 +0000

    Set-up structure for generic implementation

---
+++ Exercise 1/Serial implementation/Generic/src/Matrix_Multiply_Serial.cpp
@@ -0,0 +1,25 @@
+/**
+ * @name    Matrix_Multiply_Serial_Fixed_Values.cpp
+ * @author  B929164
+ * @date    17/12/2022
+ * @brief   This program multiplies two matrices. The program can then prints the
+ *          result to the console if that option is selected. It use the simple 
+ *          iterative algorithm to multiply the matrices. This program is a serial
+ *          version of the program and therefore does not use any parallelism and 
+ *          is limited to one thread. The values in the matrices are all intergers.
+ *          
+ *          First two matrices are created and then the values are assigned to
+ *          the matrices. The matrices are then multiplied and the result is
+ *          stored in a third matrix. The dimensions of the matrices are fixed and
+ *          can be set in by passing in the required arguments.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Hello World
+    cout << "Hello World!" << endl;
+}
